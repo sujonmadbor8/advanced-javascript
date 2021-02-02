8704b31b43fb950c91a5a5754a12b617b5b38383
sujonmadbor8 <sujonmadbor8@gmail.com>
2021-02-03 00:00:31 +0600

    null us undefined cleared for interview

---
+++ nullVsUndefined.js
@@ -0,0 +1,24 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+const result = add(25, 45);
+console.log(result)
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+const result = add(45);
+console.log(result)
+
+const premik = { name: "smart dude", phone: 458421 };
+console.log(premik.gf);
+
+
+let fun = undefined;
+console.log(fun);
+
+let array = [2, 5, 8]
+console.log(array[11]);
--- trueFalse.js
+++ trueFalse.js
@@ -1,3 +1,11 @@
+/*
+Truty:
+'0', ' ', [];
+
+Flasy:
+0, "", unefined, null, NaN(Not a Number)
+ */
+
 const age = 4;
 if (age > 0) {
     console.log("condition is true");
