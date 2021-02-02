31ab09c6a53f724fcf8c9cdf1f9d5a8210a90ff3
sujonmadbor8 <sujonmadbor8@gmail.com>
2021-02-03 00:38:27 +0600

    double us triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,16 @@
+const first = 5;
+const second = 5;
+if (first === second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+if (`2` === 2) {
+
+    console.log("Inside if");
+
+} else {
+
+    console.log("Inside else");
+
+}
