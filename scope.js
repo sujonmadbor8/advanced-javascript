5121dc1b069ab8f61276a326823154c507a3fdcf
sujonmadbor8 <sujonmadbor8@gmail.com>
2021-02-03 10:07:32 +0600

    global vs local scope

--- scope.js
+++ scope.js
@@ -4,15 +4,3 @@ function sum(first, second) {
 }
 const output = sum(3, 9);
 console.log(output);
-
-var myArr = [0, 1, 2, 3, 1000000];
-
-function getSum() {
-    var sum = 0;
-    for (var i = 0; i < 1000000; i++) {
-        sum += myArr[i];
-    }
-    return sum;
-}
-
-getSum();
